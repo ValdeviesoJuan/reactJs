a88326f0516e1cce8d9996ce3498d03c6a845e0a
ValdeviesoJuan <160289942+ValdeviesoJuan@users.noreply.github.com>
2024-05-10 00:44:15 +0800

    sidebar-header-footer-complete

--- dashboard-project/src/components/Footer.jsx
+++ dashboard-project/src/components/Footer.jsx
@@ -3,11 +3,25 @@ import React, { Component } from 'react'
 export class Footer extends Component {
   render() {
     return (
-        <footer className="bg-gray-800 text-white py-4">
-            <div className="container mx-auto px-4">
-            <p>&copy; 2024 My Footer</p>
-            </div>
-        </footer>
+      <footer className="bg-gray-900 rounded-lg shadow m-4 dark:bg-gray-800 z-0 hover:z-50">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://facebook.com/" className="hover:underline">JuanCarlos™</a>. All Rights Reserved.</span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">About</a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
     )
   }
 }
--- dashboard-project/src/components/Sidebar.jsx
+++ dashboard-project/src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 
 function Sidebar() {
     const location = useLocation();
-    const activeClass = 'rounded-md bg-gray-600 text-white p-2';
+    const activeclassName = 'bg-blue-600';
 
     const NavLinkFunction = () => {
         const sideBarArray = new Map([
@@ -14,27 +14,48 @@ function Sidebar() {
         ]);
         
         return (
-                <ul>
-                {[...sideBarArray].map(([key, value]) => (
-                  <li key={value} className="mb-2 ml-5 ">
-                    <NavLink
-                      exact= "true"
-                      to={value}
-                      className={`text-blue-500 ${location.pathname === value ? activeClass : ''}`}
-                    >
-                      {key}
-                    </NavLink>
-                  </li>
-                ))}
-              </ul>
-          
+          <ul>
+            {[...sideBarArray].map(([key, value]) => (
+              <NavLink exact="true" to={value} classNameName={`text-green ${location.pathname === value ? activeclassName : ''}`}>
+                <div key={value} className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white" >
+                  <span className="text-[15px] ml-4 text-gray-200 font-bold">{key}</span>
+                </div>
+              </NavLink>
+            ))}
+          </ul>
         );
      }
+
+    const openSidebar = () => {
+      document.querySelector(".sidebar").classList.toggle("hidden");
+    };
+
      
     return (
-      <aside className="bg-gray-200 w-64 px-4 py-8">
-        <h2 className="text-lg font-semibold mb-4">Sidebar</h2>
-        {NavLinkFunction()}
+      <aside className="bg-gray-200 w-64 px-4 py-8 flex z-10">
+        <span className="absolute text-white text-4xl top-5 left-4 cursor-pointer mt-[50px]" onClick={openSidebar}>
+          <i className="bi bi-filter-left px-2 bg-gray-900 rounded-md"></i>
+        </span>
+        <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[250px] overflow-y-auto text-center bg-gray-800">
+          <div className="text-gray-100 text-xl mt-[50px]">
+            <div className="p-2.5 flex items-center ">
+              <i className="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600"></i>
+              <h1 className="font-bold text-gray-200 text-[15px] ml-3">Sidebar</h1>
+              <i className="bi bi-x cursor-pointer ml-24 lg:hidden" onClick={openSidebar}></i>
+            </div>
+            <div className="my-2 bg-gray-600 h-[1px]"></div>
+          </div>
+
+          <div className="p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-gray-700 text-white">
+            <i className="bi bi-search text-sm"></i>
+            <input type="text" placeholder="Search" className="text-[15px] ml-4 w-full bg-transparent focus:outline-none"/>
+          </div>
+
+          {NavLinkFunction()}
+
+          <div className="my-4 bg-gray-600 h-[1px]"></div>
+        </div>
+        
       </aside>
     );
   }
